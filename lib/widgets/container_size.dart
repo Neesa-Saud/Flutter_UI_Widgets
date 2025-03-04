89734f453197a1794d63b19e929d84f2c9ac4c97
Nisha Saud <nishasud2061@gmail.com>
2025-03-04 15:45:28 +0545

    added

--- lib/widgets/container_size.dart
+++ lib/widgets/container_size.dart
@@ -6,9 +6,20 @@ class ContainerSizebox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.deepOrangeAccent,
-            title: Text(
-                'Container and SizedBox'))); //roof of app (starting of my app)
+      appBar: AppBar(
+          backgroundColor: Colors.deepOrangeAccent,
+          title: Text('Container and SizedBox')),
+      body: Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(blurRadius: 20, spreadRadius: 5, color: Colors.white)
+              ]),
+          color: Colors.amberAccent),
+      // { body:Center(child:SizedBox(height: 50,width: 50,child: Text('Hello'))) } THIS IS SIZE BOX
+    ); //roof of app (starting of my app)
   }
 }
