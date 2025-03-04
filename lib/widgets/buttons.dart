1a50a89a40e7e7fafe1dc2e84c6bcc68d8f9a968
Nisha Saud <nishasud2061@gmail.com>
2025-03-04 17:20:06 +0545

    added

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -11,12 +11,26 @@ class Buttons extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextButton(
-            onPressed: () {},
-            child: Text('Press me'),
-            style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.deepOrangeAccent)),
-          ) //(onpresses,child)these two are most required things in textButton
+              onPressed: () {},
+              child: Text('Press me'),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.deepOrangeAccent)),
+              overlayColor: MaterialStateProperty.all(Colors
+                  .brown)), //(onpresses,child)these two are most required things in textButton
+          SizedBox(
+              height: 20), //used sizeBox to add some space betwwen two buttons
+          Column(
+            //here we wrapped elevated button inside column to set height and width
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print('Elevated button pressed');
+                },
+                child: Text('Press me'),
+              ),
+            ],
+          )
         ],
       ),
     );
