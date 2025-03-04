46037d9f46cf343d799483db84fe1de3ca42323a
Nisha Saud <nishasud2061@gmail.com>
2025-03-04 15:28:35 +0545

    added files

---
+++ lib/widgets/container_size.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ContainerSizebox extends StatelessWidget {
+  const ContainerSizebox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.deepOrangeAccent,
+            title: Text(
+                'Container and SizedBox'))); //roof of app (starting of my app)
+  }
+}
