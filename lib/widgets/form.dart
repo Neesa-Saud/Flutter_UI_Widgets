5aded8da5ad736db6d95c88958dfc403556c9483
Nisha Saud <nishasud2061@gmail.com>
2025-03-08 22:14:40 +0545

    added

--- lib/widgets/form.dart
+++ lib/widgets/form.dart
@@ -8,21 +8,111 @@ class FormWidget extends StatefulWidget {
 }
 
 class _FormWidgetState extends State<FormWidget> {
-  final _formkey = GlobalKey<FormState>();
+  final _formkey = GlobalKey<FormState>(); //Form key
+  String firstname = ''; //Variables
+  String lastname = '';
+  String email = '';
+  String password = '';
+  //Functions
+  trysubmit() {
+    final isvalid = _formkey.currentState!.validate(); //checking validation
+    if (isvalid) {
+      _formkey.currentState!.save(); //saving the form
+      submitform();
+    } else {
+      print('Error');
+    }
+  }
+
+  submitform() {
+    //submitting form
+    print('firstname');
+    print('lastname');
+    print('email');
+    print('password');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Form Widget'),
+      appBar: AppBar(
+        title: Text('Form Widget'),
+      ),
+      body: Center(
+          child: Container(
+        margin: EdgeInsets.all(20),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(hintText: 'First Name'),
+                key: ValueKey('firstname'),
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return 'Please enter your first name';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  //the value entered in the form field is stored in the variable
+                  firstname = value.toString();
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'First Name'),
+                key: ValueKey('lastname'),
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return 'Please enter your last name';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  firstname = value.toString();
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'First Name'),
+                key: ValueKey('email'),
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return 'Please enter your email';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  firstname = value.toString();
+                },
+              ),
+              TextFormField(
+                obscureText: true, //makes password hidden while typing
+                decoration: InputDecoration(hintText: 'Password'),
+                key: ValueKey('password'),
+                validator: (value) {
+                  if (value.toString().length < 6) {
+                    return 'Minimum 6 characters required';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  firstname = value.toString();
+                },
+              ),
+              TextButton(
+                  //Submit button
+                  onPressed: () {
+                    trysubmit();
+                  },
+                  child: Text('Submit'))
+            ],
+          ),
         ),
-        body:Center(child: Form(child: TextFormField(key: ValueKey('firstname'),validator: (value){
-          if(value.toString().isEmpty){
-            return 'First name can't be empty';
-          }
-          else{
-            return null;
-          }
-        },)),
-        ));
+      )),
+    );
   }
 }
