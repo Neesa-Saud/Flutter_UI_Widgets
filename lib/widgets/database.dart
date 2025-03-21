dbccf97a065fd6fca4bc357599a5960aa907e843
Neesa-Saud <nishasaud2061@gmail.com>
2025-03-21 19:22:10 +0545

    added

--- lib/widgets/database.dart
+++ lib/widgets/database.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class DatabaseTime extends StatefulWidget {
@@ -13,7 +14,24 @@ class _DatabaseTimeState extends State<DatabaseTime> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Database Options"),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(Icons.leave_bags_at_home))
+        ],
         backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Create')),
+            ElevatedButton(onPressed: () {}, child: Text('Create')),
+            ElevatedButton(onPressed: () {}, child: Text('Create')),
+            ElevatedButton(onPressed: () {}, child: Text('Create'))
+          ],
+        ),
       ),
     );
   }
